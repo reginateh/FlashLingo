116f292c5c63ff586fe26c867ea11d763d91fb45
reginateh <e1312290@u.nus.edu>
2024-06-01 19:21:33 +0800

    add current page effect to the navigation tab

--- app/components/NavigationTab.tsx
+++ app/components/NavigationTab.tsx
@@ -1,5 +1,5 @@
 import { View, Text, SafeAreaView } from "react-native";
-import { Link } from "expo-router";
+import { Link, usePathname } from "expo-router";
 import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 
 type NavigationTabProps = {
@@ -8,7 +8,14 @@ type NavigationTabProps = {
   icon: "card-multiple" | "medal" | "calendar-check" | "face-man-shimmer";
 };
 
+/**
+ * A navigation tab component
+ * containing 4 tabs: Flashcards, Leaderboard, Planner, and Profile.
+ * Pages for the tabs are placed in the screens folder.
+ */
 const NavigationTab = () => {
+  const pathname = usePathname();
+
   const tabs: NavigationTabProps[] = [
     { name: "Flashcards", path: "/screens/homeScreen", icon: "card-multiple" },
     { name: "Leaderboard", path: "/screens/leaderboardScreen", icon: "medal" },
@@ -22,14 +29,25 @@ const NavigationTab = () => {
 
   return (
     <SafeAreaView className="flex-row justify-around items-center py-2 w-full bg-[#B9CBB1]">
-      {tabs.map((tab) => (
-        <Link href={tab.path} key={tab.name}>
-          <View className="flex items-center justify-center">
-            <MaterialCommunityIcons name={tab.icon} size={30} color="black" />
-            <Text>{tab.name}</Text>
-          </View>
-        </Link>
-      ))}
+      {tabs.map((tab) => {
+        const isActive = pathname === tab.path;
+        return (
+          <Link
+            href={tab.path}
+            key={tab.name}
+            style={{ pointerEvents: isActive ? "none" : "auto" }}
+          >
+            <View className="flex items-center justify-center">
+              <MaterialCommunityIcons
+                name={tab.icon}
+                size={30}
+                color={isActive ? "black" : "#99AF96"}
+              />
+              <Text>{tab.name}</Text>
+            </View>
+          </Link>
+        );
+      })}
     </SafeAreaView>
   );
 };
--- app/screens/profileScreen.tsx
+++ app/screens/profileScreen.tsx
@@ -6,6 +6,10 @@ import { onAuthStateChanged, User, updateProfile } from "firebase/auth";
 import Button from "../components/button";
 import { router } from "expo-router";
 
+/**
+ * The profile screen displays the user's display name and a logout button.
+ * It also sets the user's display name to their email if it is not set.
+ */
 const ProfileScreen = () => {
   const [user, setUser] = useState<User | null>(FIREBASE_AUTH.currentUser);
 
@@ -29,7 +33,9 @@ const ProfileScreen = () => {
   return (
     <View className="flex-1">
       <View className="flex-1 justify-center items-center">
-        <Text className="font-bold text-2xl text-center mb-2">Welcome, {user?.displayName}</Text>
+        <Text className="font-bold text-2xl text-center mb-2">
+          Welcome, {user?.displayName}
+        </Text>
         <Button
           bg="dg"
           size="md"
