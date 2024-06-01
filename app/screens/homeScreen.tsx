9c741db8129e1faff2f8acc4ff67c7c5da3e541e
reginateh <e1312290@u.nus.edu>
2024-06-01 18:57:28 +0800

    update profile screen

--- app/screens/homeScreen.tsx
+++ app/screens/homeScreen.tsx
@@ -1,7 +1,5 @@
 import { View, Text } from "react-native";
-import Button from "../components/button";
 import { FIREBASE_AUTH } from "@/firebaseConfig";
-import { router } from "expo-router";
 import NavigationTab from "../components/navigationTab";
 
 const HomeScreen = () => {
@@ -10,15 +8,6 @@ const HomeScreen = () => {
     <View className="flex-1 w-full">
       <View className="flex-1 justify-center items-center">
         <Text className="font-bold text-2xl text-center mb-2">Home page</Text>
-        <Button
-          bg="dg"
-          size="md"
-          text="Logout"
-          onPress={async () => {
-            await FIREBASE_AUTH.signOut();
-            router.navigate("/");
-          }}
-        />
       </View>
       <NavigationTab />
     </View>
--- app/screens/profileScreen.tsx
+++ app/screens/profileScreen.tsx
@@ -1,10 +1,50 @@
 import { View, Text } from "react-native";
+import { useState, useEffect } from "react";
 import NavigationTab from "../components/navigationTab";
+import { FIREBASE_AUTH } from "@/firebaseConfig";
+import { onAuthStateChanged, User, updateProfile } from "firebase/auth";
+import Button from "../components/button";
+import { router } from "expo-router";
 
 const ProfileScreen = () => {
+  const [user, setUser] = useState<User | null>(FIREBASE_AUTH.currentUser);
+
+  useEffect(() => {
+    onAuthStateChanged(FIREBASE_AUTH, async (user) => {
+      if (user) {
+        setUser(user);
+        if (user.displayName == undefined) {
+          try {
+            await updateProfile(user, {
+              displayName: user.email?.split("@")[0],
+            });
+          } catch (error: any) {
+            console.log(error);
+          }
+        }
+      }
+    });
+  }, []);
+
   return (
     <View className="flex-1">
-      <Text className="flex-1">Profile</Text>
+      <View className="flex-1 justify-center items-center">
+        <Text className="font-bold text-2xl text-center mb-2">Welcome, {user?.displayName}</Text>
+        <Button
+          bg="dg"
+          size="md"
+          text="Logout"
+          onPress={async () => {
+            try {
+              await FIREBASE_AUTH.signOut();
+            } catch (error: any) {
+              console.log(error);
+              alert("sign out failed: " + error.message);
+            }
+            router.navigate("/");
+          }}
+        />
+      </View>
       <NavigationTab />
     </View>
   );
