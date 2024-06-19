adc7e4070edce3f2542bec6af6bd58d5400be114
reginateh <e1312290@u.nus.edu>
2024-06-19 12:08:20 +0800

    update layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,7 +2,14 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack>
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#B9CBB1",
+        },
+        headerTitleAlign: "left",
+      }}
+    >
       <Stack.Screen
         name="index"
         options={{
@@ -10,6 +17,11 @@ export default function RootLayout() {
         }}
       />
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="screens/loginScreen" options={{ title: "Login" }} />
+      <Stack.Screen
+        name="screens/signUpScreen"
+        options={{ title: "Create Account" }}
+      />
     </Stack>
   );
 }
